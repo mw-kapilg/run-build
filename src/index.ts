8b912f826381b16d03b88332a47ee2fa999c6e10
Kapil Gupta <kapilg@mathworks.com>
2024-05-02 03:32:54 +0530

    Add raw HTML for table

--- src/index.ts
+++ src/index.ts
@@ -73,6 +73,25 @@ async function run() {
     // .addLink('View detailed test result', 'https://github.com')
     .write()
 
+    await core.summary
+    .addRaw(`<table>
+    <tr>
+      <th>Company</th>
+      <th>Contact</th>
+      <th>Country</th>
+    </tr>
+    <tr>
+      <td>Alfreds Futterkiste</td>
+      <td>Maria Anders</td>
+      <td>Germany</td>
+    </tr>
+    <tr>
+      <td>Centro comercial Moctezuma</td>
+      <td>Francisco Chang</td>
+      <td>Mexico</td>
+    </tr>
+  </table>`)
+
     // Cleanup post run for self hosted runners
     // await io.rmRF(workspaceDir + '/.matlab');
 
