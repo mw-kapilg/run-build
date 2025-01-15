4e6bb7b8f749b3ffe58c9c39b4f64e8212128c44
Kapil Gupta <kapilg@mathworks.com>
2025-01-15 17:51:07 +0530

    remove ignore

--- src/index.ts
+++ src/index.ts
@@ -233,16 +233,14 @@ async function run() {
       <td valign="top" align="center">0.01</td>
     </tr>
     </table>
-    <![CDATA[
-    <script>
+    <script >
       function toggleColumns() {
         const columns = document.querySelectorAll('.toggle-column');
         columns.forEach(column => {
             column.classList.toggle('hidden');
         });
       }
-    </script>
-    ]]>`;
+    </script>`;
     const myStackTrace = `<details>c<h3><b>❌ <u>TestFile3/Test1 failed</u></b></h3>
         <details><summary>View stack trace</summary></br>
         <pre>Verification failed in TestExamples/testNonLeapYear.\n    ---------------------\n    Framework Diagnostic:\n    ---------------------\n    verifyEqual failed.\n    --> The numeric values are not equal using \"isequaln\".\n    --> Failure table:\n            Actual    Expected    Error    RelativeError\n            ______    ________    _____    _____________\n              1          2         -1          -0.5    \n    Actual Value:\n         1\n    Expected Value:\n         2\n    ------------------\n    Stack Information:\n    ------------------\n    In C:\\Users\\kapilg\\jenkins visualization\\test-results\\jenkins-matlab-plugin\\work\\workspace\\visualization\\tests\\TestExamples.m (TestExamples.testNonLeapYear) at 36</pre>
