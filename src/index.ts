5c4fb4d4b189d690a592f34f6ca892601e27f32a
Kapil Gupta <kapilg@mathworks.com>
2025-01-15 18:06:43 +0530

    add collapse expand

--- src/index.ts
+++ src/index.ts
@@ -217,7 +217,7 @@ async function run() {
         });
       }
     </script>`;
-    const myStackTrace = `<details><h3><b>❌ <u>TestFile3/Test1 failed</u></b></h3>
+    const myStackTrace = `<details><summary>View all failed tests</summary><h3><b>❌ <u>TestFile3/Test1 failed</u></b></h3>
         <details><summary>View stack trace</summary></br>
         <pre>Verification failed in TestExamples/testNonLeapYear.\n    ---------------------\n    Framework Diagnostic:\n    ---------------------\n    verifyEqual failed.\n    --> The numeric values are not equal using \"isequaln\".\n    --> Failure table:\n            Actual    Expected    Error    RelativeError\n            ______    ________    _____    _____________\n              1          2         -1          -0.5    \n    Actual Value:\n         1\n    Expected Value:\n         2\n    ------------------\n    Stack Information:\n    ------------------\n    In C:\\Users\\kapilg\\jenkins visualization\\test-results\\jenkins-matlab-plugin\\work\\workspace\\visualization\\tests\\TestExamples.m (TestExamples.testNonLeapYear) at 36</pre>
         </details></details>
